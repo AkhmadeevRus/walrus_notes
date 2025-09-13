3a278fc767a1e14f60a1e22cc4f755f64b1f6883
Akhmadeev Ruslan <akhmadeevrusl@gmail.com>
2025-09-13 22:24:19 +0700

    я все понял и осознал(больше не панк)

--- app/application.go
+++ app/application.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -12,6 +13,10 @@ type Note struct {
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"createdAt"`
+}
+
+type App struct {
+	Notes []Note
 }
 
 func NewNote(title string, description string) *Note {
@@ -22,16 +27,20 @@ func NewNote(title string, description string) *Note {
 	}
 }
 
-var funcTable = map[string]menuFunction{
-	"0": menuFunction{Title: "Выход", Function: StopApp},
-	"1": menuFunction{Title: "Тестовая функция с ошибкой", Function: TestFuncWithError},
-	"2": menuFunction{Title: "Тестовая успешная функция", Function: AccessFunc},
-	"3": menuFunction{Title: "Добавить новую запись", Function: AddNote},
-	"4": menuFunction{Title: "вывести все записи", Function: ListNotes},
-	"5": menuFunction{Title: "изменить выбранную заметку", Function: UpdateNote},
-}
-
 func RunApp() {
+	var notes = App{Notes: []Note{}}
+	err := notes.ReadNotesFromFile()
+	if err != nil {
+		fmt.Println(err)
+	}
+	var funcTable = map[string]menuFunction{
+		"0": menuFunction{Title: "Выход", Function: notes.StopApp},
+		"1": menuFunction{Title: "Тестовая функция с ошибкой", Function: TestFuncWithError},
+		"2": menuFunction{Title: "Тестовая успешная функция", Function: AccessFunc},
+		"3": menuFunction{Title: "Добавить новую запись", Function: notes.AddNote},
+		"4": menuFunction{Title: "вывести все записи", Function: notes.ListNotes},
+		"5": menuFunction{Title: "изменить выбранную заметку", Function: notes.UpdateNote},
+	}
 	fmt.Println("\033[33mДобро пожаловать \"Записки Ластоногих\"\033[0m")
 	var command string
 	for {
@@ -62,13 +71,17 @@ func TestFuncWithError() error {
 	return errors.New("тестовая ошибка")
 }
 
-func StopApp() error {
+func (notes *App) StopApp() error {
+	err := notes.WriteNotesToFile()
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println("\033[33mДо свидания!\033[0m")
 	os.Exit(0)
 	return nil
 }
 
-func AddNote() error {
+func (notes *App) AddNote() error {
 	var title, description string
 	scan := bufio.NewScanner(os.Stdin)
 	for scan.Scan() {
@@ -81,97 +94,73 @@ func AddNote() error {
 	scan.Scan()
 	description = scan.Text()
 	note := NewNote(title, description)
-	notes, err := ReadNotesFromFile()
-	if err != nil {
-		fmt.Println(err)
-	}
-	notes = append(notes, *note)
-	err = WriteNotesToFile(notes)
-	if err != nil {
-		fmt.Print(err)
-	}
+	notes.Notes = append(notes.Notes, *note)
 	return nil
 }
 
-func ListNotes() error {
-	notes, err := ReadNotesFromFile()
-	if err != nil {
-		fmt.Printf("Ошибка при чтении из json файла %s", err)
-		return err
-	}
+func (notes *App) ListNotes() error {
 	var command int
 	for {
-		fmt.Println("вы можете увидеть подробное описание заметки, введя ее номер:")
-		generateListMenu(notes)
+		generateListMenu(notes.Notes)
 		fmt.Println("0 - закончить просмотр записей")
+		fmt.Print("вы можете увидеть подробное описание заметки, введя ее номер:")
 		_, err := fmt.Scan(&command)
-		if err != nil || command > len(notes) {
-			fmt.Println("\033[31mКоманда не найдена\033[0m")
-			return errors.New("команда не найдена")
+		if err != nil || command > len(notes.Notes) {
+			fmt.Println("\n\033[31mКоманда не найдена\033[0m")
+			continue
 		}
 		if command == 0 {
 			break
 		}
-		fmt.Printf("%d - %s - %s\n%-100s\n", command, notes[command-1].Title, notes[command-1].CreatedAt.Format("01.02.2006"), notes[command-1].Description)
+		fmt.Printf("%d - %s - %s\n%-100s\n", command, notes.Notes[command-1].Title, notes.Notes[command-1].CreatedAt.Format("01.02.2006"), notes.Notes[command-1].Description)
 	}
 	return nil
 }
 
-func UpdateNote() error {
-	notes, err := ReadNotesFromFile()
-	if err != nil {
-		fmt.Printf("Ошибка при чтении из json файла %s", err)
+func (notes *App) UpdateNote() error {
+	scan := bufio.NewScanner(os.Stdin)
+	generateListMenu(notes.Notes)
+	fmt.Print("введите номер заметки, которую хотите изменить: ")
+	for scan.Scan() {
+		break
 	}
-	var command int
-	for {
-		generateListMenu(notes)
-		fmt.Println("0 - закончить просмотр записей")
-		fmt.Print("введите номер заметки, которую хотите изменить: ")
-		_, err := fmt.Scan(&command)
-		if err != nil || command > len(notes) {
-			fmt.Println("\033[31mКоманда не найдена\033[0m")
-			return nil
-		}
-		if command == 0 {
-			break
-		}
-		scan := bufio.NewScanner(os.Stdin)
-		for scan.Scan() {
-			break
-		}
-		fmt.Println("введите новый title для заметки: ")
-		scan.Scan()
-		title := scan.Text()
-		if title == "" {
-			fmt.Println("Текст заголовка не изменен!")
-		} else {
-			notes[command-1].Title = title
-			fmt.Println("Текст заголовка изменен!")
-		}
-		fmt.Println("введите новой description для заметки: ")
-		scan.Scan()
-		description := scan.Text()
-		if description == "" {
-			fmt.Println("Текст заметки не изменен!")
-		} else {
-			notes[command-1].Description = description
-			fmt.Println("Текст заметки изменен!")
-		}
-		fmt.Println("введите новою дату для заметки (в формате 01.01.2000): ")
-		scan.Scan()
-		date := scan.Text()
-		if date != "" {
-			ParseDate, err := time.Parse("01.02.2006", date)
-			if err != nil {
-				fmt.Println("неверный формат даты!")
-			} else {
-				notes[command-1].CreatedAt = ParseDate
-				fmt.Println("дата изменена")
-			}
-		} else {
-			fmt.Println("дата не изменена")
-		}
+	scan.Scan()
+	command := scan.Text()
+	idx, err := strconv.Atoi(command)
+	if err != nil || idx > len(notes.Notes) || idx < 1 {
+		return errors.New("\033[31mКоманда не найдена\033[0m")
 	}
-	WriteNotesToFile(notes)
+	fmt.Println("введите новый title для заметки: ")
+	scan.Scan()
+	title := scan.Text()
+	if title == "" {
+		fmt.Println("Текст заголовка не изменен!")
+	} else {
+		notes.Notes[idx-1].Title = title
+		fmt.Println("Текст заголовка изменен!")
+	}
+	fmt.Println("введите новой description для заметки: ")
+	scan.Scan()
+	description := scan.Text()
+	if description == "" {
+		fmt.Println("Текст заметки не изменен!")
+	} else {
+		notes.Notes[idx-1].Description = description
+		fmt.Println("Текст заметки изменен!")
+	}
+	fmt.Println("введите новою дату для заметки (в формате 01.01.2000): ")
+	scan.Scan()
+	date := scan.Text()
+	if date != "" {
+		ParseDate, err := time.Parse("01.02.2006", date)
+		if err != nil {
+			fmt.Println("неверный формат даты!")
+		} else {
+			notes.Notes[idx-1].CreatedAt = ParseDate
+			fmt.Println("дата изменена")
+		}
+	} else {
+		fmt.Println("дата не изменена")
+	}
 	return nil
 }
--- app/file.go
+++ app/file.go
@@ -2,60 +2,46 @@ package app
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 )
 
-func getNotesFilePath() string {
-	path, err := os.Getwd()
-	if err != nil {
-		fmt.Printf("ошибка при взятии пути дериктории: %s", err)
-	}
-	path += "\\note.json"
-	return path
-}
-
-func ReadNotesFromFile() ([]Note, error) {
-	filePath := getNotesFilePath()
+func (notes *App) ReadNotesFromFile() error {
+	filePath := "note.json"
 	_, err := os.Stat(filePath)
 	if os.IsNotExist(err) {
 		fmt.Println("create file")
 		file, err := os.Create(filePath)
 		if err != nil {
-			fmt.Printf("ошибка при сoздании файла: %s\n", err)
-			return nil, err
+			return err
 		}
 		defer file.Close()
 		os.WriteFile(filePath, []byte("[]"), 0644)
-		return []Note{}, nil
+		return nil
 	}
 	file, err := os.Open(filePath)
 	if err != nil {
-		fmt.Printf("ошбка при открытии файла(read): %s", err)
-		return nil, err
+		return err
 	}
 	defer file.Close()
-	notes := []Note{}
-	err = json.NewDecoder(file).Decode(&notes)
+	err = json.NewDecoder(file).Decode(&notes.Notes)
 	if err != nil {
-		fmt.Printf("ошибка при чтении из json файла: %s", err)
-		return nil, err
+		return err
 	}
-	return notes, nil
+	return nil
 }
 
-func WriteNotesToFile(notes []Note) error {
-	filePath := getNotesFilePath()
+func (notes *App) WriteNotesToFile() error {
+	filePath := "note.json"
 	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
-		fmt.Printf("ошибка при открытии файла(write): %s", err)
-		return err
+		return errors.New("ошибка при записи в файл, ты потерял все данные!!!:(")
 	}
 	defer file.Close()
-	err = json.NewEncoder(file).Encode(notes)
+	err = json.NewEncoder(file).Encode(notes.Notes)
 	if err != nil {
-		fmt.Printf("ошибка при записи в файл: %s", err)
-		return err
+		return errors.New("ошибка при записи в файл, ты потерял все данные!!!:(")
 	}
 	return nil
 }
